6abc4d5daa72f0884a6b5c5cf207204e0df44119
Marion1b <121030423+Marion1b@users.noreply.github.com>
2024-08-14 11:28:04 +0200

    feat : footer (#2)

---
+++ cfu-conseils/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <footer className="page-footer">
+            <section className="page-footer-container">
+                <p>CFU conseils</p>
+                <p>Mentions légales</p>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
