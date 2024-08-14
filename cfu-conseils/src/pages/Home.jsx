433f07c7077d297d2132a4cc7c7d2d0f7f8e69e8
Marion1b <121030423+Marion1b@users.noreply.github.com>
2024-08-14 15:05:53 +0200

    Feature/home page (#5)
    * wave
    
    * feat : home page

--- cfu-conseils/src/pages/Home.jsx
+++ cfu-conseils/src/pages/Home.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import Button from '../components/Button.jsx';
+import waveHome from '../assets/wave-home.svg';
+import photo from '../assets/photo.png';
 
 function Home(){
     return(
         <main className='home'>
-            <h1>Home</h1>
-            <Button content="Nos services" color="light"/>
+            <section className="home-presentation">
+                <h1>Comptabilité et conseils en gestion</h1>
+                <p>CFU conseils est là pour vous aider à gérer efficacement vos finances et à prendre  des décisions éclairées pour votre entreprise. Nous sommes une équipe de  professionnels expérimentés et passionnés, déterminés à fournir un  service personnalisé et de qualité à chaque client.</p>
+                <Button content="Nos services" color="orange"/>
+                <Button content="Nous contacter" color="secondary"/>
+            </section>
+            <figure><img src={waveHome} alt="" className='wave-home'/></figure>
+            <section className="home-services">
+                <h1>Nos services</h1>
+                <section className="home-services-cards-container">
+                    <article className="home-services-card">
+                        <h2>Comptabilité gestion locative</h2>
+                        <p>Gestion des tâches administratives liées à la location de vos biens immobiliers.</p>
+                        <Button content="En savoir plus" color="orange"/>
+                    </article>
+                    <article className="home-services-card">
+                        <h2>Comptabilité d'une société</h2>
+                        <p>Tenue complète de la comptabilité d’une société. A destination des TPE, associations, indépendants, libéraux...</p>
+                        <Button content="En savoir plus" color="orange"/>
+                    </article>
+                    <article className="home-services-card">
+                        <h2>Conseils en gestion</h2>
+                        <p>Conseils pour la fraude fiscale et ne pas payer vos impôts pour devenir aussi riche que Bernard Arnault.</p>
+                        <Button content="En savoir plus" color="orange"/>
+                    </article>
+                </section>
+            </section>
+            <section className="home-team">
+                <h1>Qui sommes-nous ?</h1>
+                <section className="home-team-cards-container">
+                    <article className="home-team-card">
+                        <img src={photo} alt="profil de Catherine" />
+                        <div>
+                            <p className="bold">Catherine</p>
+                            <p>Comptable</p>
+                        </div>
+                    </article>
+                    <article className="home-team-card">
+                        <img src={photo} alt="profil de François" />
+                        <div>
+                            <p className="bold">François</p>
+                            <p>Conseiller en gestion</p>
+                        </div>
+                    </article>
+                </section>
+            </section>
         </main>
     )
 }
