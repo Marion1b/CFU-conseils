35cb8cad3f1022bfd68bd50c0fc8bc3a4142b8b6
Marion1b <marion.barthoux@gmail.com>
2024-08-20 10:30:03 +0200

    feat : services pages

---
+++ cfu-conseils/src/pages/ComptaGestion.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from '../components/Button.jsx';
+import immobilierImage from '../assets/immobilier.svg';
+import waveImmo from '../assets/waveImmo.svg';
+
+function ComptaGestion(){
+    return(
+        <main className="service service-immo">
+            <section className="service-presentation">
+                <article className="service-presentation-text">
+                    <h1>Comptabilité gestion locative immobilière</h1>
+                    <p>Un service de comptabilité de gestion locative immobilière est un service qui aide les propriétaires d'immeubles à gérer les aspects financiers de leur propriété locative. Ce service peut inclure la tenue de la comptabilité des loyers perçus et des dépenses liées à la propriété, la préparation de rapports financiers périodiques, la gestion des factures et des paiements, ainsi que la préparation de déclarations fiscales. Les propriétaires peuvent ainsi avoir une vue d'ensemble claire de la performance financière de leur propriété et prendre des décisions éclairées en conséquence. Ce service peut également aider les propriétaires à respecter les réglementations fiscales et comptables en vigueur, ce qui peut les aider à éviter des problèmes juridiques et financiers potentiels. En somme, un service de comptabilité de gestion locative immobilière peut être un outil précieux pour les propriétaires qui cherchent à maximiser la rentabilité de leur propriété tout en respectant les règles et les réglementations en vigueur.</p>
+                    <Button content="Nous contacter" color="orange"/>
+                    <Button content="En savoir plus" color="secondary"/>
+                </article>
+                <aside>
+                    <img src={immobilierImage} alt="" />
+                </aside>
+            </section>
+            <figure><img src={waveImmo} alt="" className="wave-immobilier"/></figure>
+            <section className="service-more">
+                <article className="service-more-container">
+                    <h2>Que contient ce service ?</h2>
+                    <article className="service-more-explain">
+                        <div>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                        </div>
+                        <div>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                        </div>
+                    </article>
+                    <p>Si vous êtes intéressés uniquement par une partie de ces services, n'hésitez pas à nous contacter. Nous serons ravis de discuter de vos  besoins et de vous proposer une offre adaptée à vos attentes.</p>
+                    <Button content="Nous contacter" color="orange"/>
+                </article>
+            </section>
+        </main>
+    )
+}
+
+export default ComptaGestion;
---
+++ cfu-conseils/src/pages/Conseil.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from '../components/Button.jsx';
+import conseilImage from '../assets/conseil.svg';
+import waveImmo from '../assets/waveImmo.svg';
+
+function Conseil(){
+    return(
+        <main className="service service-societe">
+            <section className="service-presentation">
+                <article className="service-presentation-text">
+                    <h1>Conseils en gestion</h1>
+                    <p>Un service de conseils en gestion est un service qui aide les entreprises à améliorer leur performance et leur efficacité opérationnelle. Ce service peut inclure des conseils en matière de stratégie d'entreprise, de gestion des coûts, d'optimisation des processus métier, de gestion des ressources humaines, de gestion de la chaîne d'approvisionnement, de gestion de la qualité, de gestion des risques, etc. Les conseillers en gestion travaillent souvent en étroite collaboration avec les équipes de direction de l'entreprise pour comprendre leurs besoins et leurs défis, puis élaborent des plans d'action personnalisés pour les aider à atteindre leurs objectifs. Les entreprises peuvent ainsi bénéficier d'une expertise externe pour les aider à prendre des décisions stratégiques éclairées, à améliorer leur performance et à atteindre leurs objectifs de croissance et de rentabilité. En somme, un service de conseils en gestion peut être un atout précieux pour les entreprises qui cherchent à améliorer leur performance et leur efficacité opérationnelle.</p>
+                    <Button content="Nous contacter" color="orange"/>
+                    <Button content="En savoir plus" color="secondary"/>
+                </article>
+                <aside>
+                    <img src={conseilImage} alt="" />
+                </aside>
+            </section>
+            <figure><img src={waveImmo} alt="" className="wave-immobilier"/></figure>
+            <section className="service-more">
+                <article className="service-more-container">
+                    <h2>Que contient ce service ?</h2>
+                    <article className="service-more-explain">
+                        <div>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                        </div>
+                        <div>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                            <p><span className="bold">Comptabilité générale</span> : nous vous accompagnons dans le suivi de l'ensemble des transactions  financières de votre entreprise, en préparant vos bilans et comptes de  résultat de manière rigoureuse et en vous fournissant une vision claire  et précise de votre situation financière.</p>
+                        </div>
+                    </article>
+                    <p>Si vous êtes intéressés uniquement par une partie de ces services, n'hésitez pas à nous contacter. Nous serons ravis de discuter de vos  besoins et de vous proposer une offre adaptée à vos attentes.</p>
+                    <Button content="Nous contacter" color="orange"/>
+                </article>
+            </section>
+        </main>
+    )
+}
+
+export default Conseil;
