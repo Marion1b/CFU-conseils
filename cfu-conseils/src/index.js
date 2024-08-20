2905cd6783cb2e445ee6088b757a9ba8f1c9312a
Marion1b <marion.barthoux@gmail.com>
2024-08-20 16:44:13 +0200

    feat : responsive header

--- cfu-conseils/src/index.js
+++ cfu-conseils/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style/index.css';
 import reportWebVitals from './reportWebVitals.js';
 import Header from './components/Header.jsx';
+import HeaderMobile from './components/HeaderMobile.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
@@ -16,6 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <HeaderMobile />
     <Router>
       <Routes>
         <Route path = "/" element={<Home/>}/>
